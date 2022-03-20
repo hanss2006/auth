0dd8c0c90d74843edbb3c9e4b89a8a91fcc485aa
hanss <hanss@list.ru>
2022-03-20 19:35:08 +0300

    Initial commit

--- src/main/java/com/hanss/auth/AuthApplication.java
+++ src/main/java/com/hanss/auth/AuthApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthApplication {
 
 	public static void main(String[] args) {
+		//System.setProperty("server.servlet.context-path","/api/auth");
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
