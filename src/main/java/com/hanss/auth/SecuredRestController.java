aa3d53769c021441bae225a68ff210852e40c9a7
hanss <hanss@list.ru>
2022-04-03 22:28:17 +0300

    Initial commit

--- src/main/java/com/hanss/auth/SecuredRestController.java
+++
@@ -1,7 +0,0 @@
-package com.hanss.auth;
-
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-
-@SecurityRequirement(name = "bearerAuth")
-public interface SecuredRestController {
-}
